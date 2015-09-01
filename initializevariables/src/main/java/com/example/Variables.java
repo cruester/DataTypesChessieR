35ca9981c55a61bb4793f4d9cf3389743e784897
Chessie Ruester <cruester@e104-m-06.mchenry.edu>
2015-09-01 13:29:26 -0500

    Initial commit

---
+++ initializevariables/src/main/java/com/example/Variables.java
@@ -0,0 +1,27 @@
+package com.example;
+
+public class Variables
+{
+    public static void main(String[] args)
+    {
+        byte myByte = 97;
+        short myShort = 32000;
+        int myInt = -100000;
+        long myLong = 1234987;
+        float myFloat = 3;
+        double myDouble = 5.33;
+        boolean myBool = true;
+        char myCharacter = 'C';
+
+        System.out.println("My btye value is: " + myByte);
+        System.out.println("My short value is: " + myShort);
+        System.out.println("My integer value is: " + myInt);
+        System.out.println("My long value is: " + myLong);
+        System.out.println("My double value is: " +myDouble);
+        System.out.println("My Boolean value is: " + myBool);
+        System.out.println("My name starts with: " + myCharacter);
+
+
+    }
+
+}
